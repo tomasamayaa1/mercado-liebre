24177a078da754ea5e6567ec7a31649ae8322164
tomasamayaa1 <tomasamayaa1@gmail.com>
2022-06-10 13:09:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.listen(3000, ()=> {
+    console.log('Servidor Corriendo en puerto 3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
